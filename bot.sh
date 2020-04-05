6f24945cbc597cb10115d3aa7b5bd8a98a391fbe
Sutan Nasution <sutan.gnst@gmail.com>
2020-04-06 00:19:54 +0700

    initial commit

---
+++ bot.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+arr[0]="bot: 👋 Hello Github!"
+arr[1]="bot: 🥳 Yeayyy!"
+arr[2]="bot: 😬 Working from github."
+arr[3]="bot: 👨‍💻 Work, work, work!"
+arr[4]="bot: 😪 Hufft..."
+arr[5]="bot: 😎 I'm working for my master!"
+arr[6]="bot: 🙄 Running task, again."
+arr[7]="bot: 👻 Thanks master."
+rand=$[$RANDOM % ${#arr[@]}]
+d=`date '+%Y-%m-%dT%H:%M:%SZ'`
+
+echo "## 🤔 LAST UPDATED AT: ${d}" > update.md
+
+git config --local user.email "sutan.gnst@gmail.com"
+git config --local user.name "Sutan Nasution"
+git add -A
+
+git commit -m "${arr[$rand]} at ${d}"
